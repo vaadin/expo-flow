90494ca07e8cc59cb87328030a80d42c35061579
Marcus Hellberg <marcus@vaadin.com>
2022-04-07 10:48:33 -0700

    clean up sandbox

--- src/main/java/com/example/application/views/sandbox/SandboxView.java
+++ src/main/java/com/example/application/views/sandbox/SandboxView.java
@@ -2,30 +2,30 @@ package com.example.application.views.sandbox;
 
 import com.example.application.views.MainLayout;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
 @PageTitle("Sandbox")
 @Route(value = "sandbox", layout = MainLayout.class)
-public class SandboxView extends HorizontalLayout {
+public class SandboxView extends VerticalLayout {
 
-    private TextField name;
-    private Button sayHello;
 
     public SandboxView() {
-        name = new TextField("Your name");
-        sayHello = new Button("Say hello");
-        sayHello.addClickListener(e -> {
-            Notification.show("Hello " + name.getValue());
+        var name = new TextField("Your name");
+        var button = new Button("Say hello");
+        button.addClickListener(e -> {
+            add(new Paragraph("Hello " + name.getValue()));
         });
 
-        setMargin(true);
-        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+        var form = new HorizontalLayout(name,button);
+        form.setAlignItems(Alignment.END);
 
-        add(name, sayHello);
+        add(form);
     }
 
 }
