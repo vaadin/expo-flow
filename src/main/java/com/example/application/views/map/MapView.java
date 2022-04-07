1cc9fbafd59bb18d6ca7820fa038820f2ae2744c
Marcus Hellberg <marcus@vaadin.com>
2022-04-07 10:43:48 -0700

    add marker on map

--- src/main/java/com/example/application/views/map/MapView.java
+++ src/main/java/com/example/application/views/map/MapView.java
@@ -4,6 +4,7 @@ import com.example.application.views.MainLayout;
 import com.vaadin.flow.component.map.Map;
 import com.vaadin.flow.component.map.configuration.Coordinate;
 import com.vaadin.flow.component.map.configuration.View;
+import com.vaadin.flow.component.map.configuration.feature.MarkerFeature;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -20,8 +21,12 @@ public class MapView extends VerticalLayout {
         map.addThemeName("borderless");
 
         View view = map.getView();
-        view.setCenter(Coordinate.fromLonLat(10.0, 55.0));
+        view.setCenter(Coordinate.fromLonLat(-100, 38));
         view.setZoom(4);
+
+        var devNexus = new MarkerFeature(Coordinate.fromLonLat(-84.38, 33.75));
+        map.getFeatureLayer().addFeature(devNexus);
+
         addAndExpand(map);
     }
 }
