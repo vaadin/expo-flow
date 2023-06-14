ac0ab1c61b41938363d1859e0b2849323a6849b2
Marcus Hellberg <marcus@vaadin.com>
2023-06-14 15:25:14 -0700

    Layout examples

--- src/main/java/com/example/application/views/LayoutsView.java
+++ src/main/java/com/example/application/views/LayoutsView.java
@@ -1,7 +1,12 @@
 package com.example.application.views;
 
-import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.EmailField;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
@@ -10,6 +15,31 @@ import com.vaadin.flow.router.Route;
 public class LayoutsView extends VerticalLayout {
 
     public LayoutsView() {
-        add(new H1("LayoutsView"));
+        add(new H2("Vertical Layout"));
+        add(new VerticalLayout(
+                new Button("Button 1"),
+                new Button("Button 2"),
+                new Button("Button 3")
+        ));
+
+        add(new H2("Horizontal Layout"));
+        add(new HorizontalLayout(
+                new Button("Button 1"),
+                new Button("Button 2"),
+                new Button("Button 3")
+        ));
+
+        add(new H2("Combining layouts"));
+        add(new VerticalLayout(
+
+                new TextField("Name"),
+                new EmailField("Email"),
+                new DatePicker("Date of birth"),
+
+                new HorizontalLayout(
+                        new Button("Save"),
+                        new Button("Cancel")
+                )
+        ));
     }
 }
