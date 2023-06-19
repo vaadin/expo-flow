2d41bc893dbad902c610c9dbc298d4bc863173ba
Marcus Hellberg <marcus@vaadin.com>
2023-06-19 10:56:52 +0200

    Remove unused import

--- src/main/java/com/example/application/views/SandboxView.java
+++ src/main/java/com/example/application/views/SandboxView.java
@@ -1,14 +1,9 @@
 package com.example.application.views;
 
-import com.example.application.views.MainLayout;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.combobox.ComboBox;
-import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.html.Paragraph;
-import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.richtexteditor.RichTextEditor;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -21,11 +16,12 @@ public class SandboxView extends VerticalLayout {
     public SandboxView() {
         var name = new TextField("Your name");
         var button = new Button("Say hello");
+
         button.addClickListener(e -> {
             add(new Paragraph("Hello " + name.getValue()));
         });
 
-        var form = new HorizontalLayout(name,button);
+        var form = new HorizontalLayout(name, button);
         form.setAlignItems(Alignment.END);
 
 
