06600a8c18ffd2db6cae2f9fc96a8de63cba22ab
Manolo Carrasco <manolo@vaadin.com>
2025-10-23 14:20:28 +0200

    remove virit dependencies

--- src/main/java/com/example/application/views/ChatView.java
+++ src/main/java/com/example/application/views/ChatView.java
@@ -1,39 +1,68 @@
 package com.example.application.views;
 
-import com.vaadin.flow.component.messages.MessageInput;
-import com.vaadin.flow.component.orderedlayout.Scroller;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.vaadin.lineawesome.LineAwesomeIconUrl;
+
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.messages.MessageList;
+import com.vaadin.flow.component.messages.MessageListItem;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Menu;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import org.springframework.ai.chat.client.ChatClient;
-import org.vaadin.firitin.components.messagelist.MarkdownMessage;
-import org.vaadin.lineawesome.LineAwesomeIconUrl;
 
 @PageTitle("Chat")
 @Menu(title = "Chat", icon = LineAwesomeIconUrl.COMMENTS_SOLID, order = 4)
 @Route(value = "chat")
 public class ChatView extends VerticalLayout {
 
+    private final MessageList messageList = new MessageList();
+    private final TextField input = new TextField();
+    private final Button sendButton = new Button("Send");
+    private final List<MessageListItem> items = new ArrayList<>();
+    HorizontalLayout form = new HorizontalLayout(input, sendButton);
+
     public ChatView(ChatClient.Builder chatClientBuilder) {
         ChatClient chatClient = chatClientBuilder.build();
-        setSizeFull();
-        var messageList = new VerticalLayout();
-        var messageInput = new MessageInput();
-        messageInput.setWidthFull();
 
-        messageInput.addSubmitListener(event -> {
-            var question = event.getValue();
-            var userMessage = new MarkdownMessage(question, "You", MarkdownMessage.Color.AVATAR_PRESETS[0]);
-            var assistantMessage = new MarkdownMessage("Assistant", MarkdownMessage.Color.AVATAR_PRESETS[1]);
+        messageList.setItems(items);
 
-            messageList.add(userMessage, assistantMessage);
-
-            chatClient.prompt().user(question).stream().content()
-                    .subscribe(assistantMessage::appendMarkdownAsync);
+        sendButton.addClickShortcut(Key.ENTER);
+        sendButton.addClickListener(e -> {
+            messageList.setMarkdown(true);
+            String userText = input.getValue();
+            if (userText == null || userText.isBlank()) {
+                return;
+            }
+            MessageListItem userMsg = new MessageListItem(userText, Instant.now(), "You");
+            userMsg.setUserColorIndex(4);
+            items.add(userMsg);
+            MessageListItem aiMsg = new MessageListItem("", Instant.now(), "Assistant");
+            aiMsg.setUserColorIndex(1);
+            items.add(aiMsg);
+            UI ui = UI.getCurrent();
+            chatClient.prompt().user(userText).stream().content()
+                    .subscribe(t -> {
+                        ui.access(() -> {
+                            aiMsg.appendText(t);
+                        });
+                    });
+            messageList.setItems(items);
+            input.clear();
         });
 
-        addAndExpand(new Scroller(messageList));
-        add(messageInput);
+        setSizeFull();
+        form.expand(input);
+        form.setWidthFull();
+        messageList.setSizeFull();
+        add(messageList, form);
     }
 }
