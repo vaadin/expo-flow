58ed6f2de4ded20dac8881855207b3a4e41774c1
vaadin <expo@vaadin.com>
2026-02-04 12:17:28 +0100

    Add back playground demo

--- src/main/java/com/example/application/views/PlaygroundView.java
+++ src/main/java/com/example/application/views/PlaygroundView.java
@@ -2,8 +2,11 @@ package com.example.application.views;
 
 import com.example.application.data.entity.Person;
 import com.example.application.service.PersonService;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Menu;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -14,14 +17,15 @@ import org.vaadin.lineawesome.LineAwesomeIconUrl;
 @Menu(title = "Java Playground", icon = LineAwesomeIconUrl.CODE_SOLID, order = 5)
 public class PlaygroundView extends VerticalLayout {
 
-    public PlaygroundView(PersonService service) {
+    public PlaygroundView() {
+        var name = new TextField("Your name");
+        var button = new Button("Click me");
 
-        var grid = new Grid<>(Person.class);
-        grid.setItems(service.findAll());
-        grid.setColumns("firstName", "lastName", "email");
+        button.addClickListener(e -> {
+            add(new Paragraph("Hello, " + name.getValue()));
+        });
 
-        add(grid);
-
+        add(name, button);
     }
 
 }
